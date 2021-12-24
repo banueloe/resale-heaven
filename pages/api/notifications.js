f6780fb49b2ba304f725b691a1b3ee6f50a73858
banueloe <ernestobanuelos14@gmail.com>
2021-12-24 17:57:47 -0600

    Handle incoming ebay notifications

--- pages/api/notifications.js
+++ pages/api/notifications.js
@@ -1,17 +1,22 @@
-const crypto = require('crypto');
- export default function handler(req, res) {
-   if (req.method === "GET") {
-     const challengeCode = req.query.challenge_code;
+const crypto = require("crypto");
+export default function handler(req, res) {
+  if (req.method === "GET") {
+    //This route is for initial eBay notificaton validation
+    const challengeCode = req.query.challenge_code;
 
-     const hash = crypto.createHash("sha256");
-     hash.update(challengeCode);
-     hash.update(process.env.VERIFICATION_TOKEN);
-     hash.update(process.env.EBAY_NOTIFICATIONS_ENDPOINT);
-     const responseHash = hash.digest("hex");
-     let challengeResponse = new Buffer.from(responseHash).toString();
+    const hash = crypto.createHash("sha256");
+    hash.update(challengeCode);
+    hash.update(process.env.VERIFICATION_TOKEN);
+    hash.update(process.env.EBAY_NOTIFICATIONS_ENDPOINT);
+    const responseHash = hash.digest("hex");
+    let challengeResponse = new Buffer.from(responseHash).toString();
 
-     res.status(200).json({ challengeResponse: challengeResponse });
-   } else {
-     res.status(405).json({ error: "This endpoint only accepts GET requests" });
-   }
- }
+    res.status(200).json({ challengeResponse: challengeResponse });
+  } else if (req.method === "POST") {
+    //This route handles ongoing (incoming) notifications
+    console.log(req.body);
+    res.status(200).json({ message: "Notication received: User account data deleted" });
+  } else {
+    res.status(405).json({ error: "This endpoint only accepts GET requests" });
+  }
+}
