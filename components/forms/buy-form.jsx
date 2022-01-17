3cb2a32e8338ab9815289bd60bd4b9c083feca01
banueloe <ernestobanuelos14@gmail.com>
2022-01-16 22:07:38 -0600

    Support used product searches

--- components/forms/buy-form.jsx
+++ components/forms/buy-form.jsx
@@ -1,11 +1,22 @@
 import { useState } from "react";
-import { Grid, Button, TextField } from "@mui/material";
+import {
+  Grid,
+  Button,
+  TextField,
+  Radio,
+  RadioGroup,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+} from "@mui/material";
 
 const BuyForm = ({ setActiveItems }) => {
-  const [keywords, setKeywords] = useState("");
+  const [keywords, setKeywords] = useState();
+  const [conditionValue, setConditionValue] = useState("new");
 
-  const getActiveItems = (keywords) => {
-    fetch(`/api/activeitems?keywords=${keywords}`)
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`/api/activeitems?keywords=${keywords}&condition=${conditionValue}`)
       .then((response) => response.json())
       .then((data) => {
         setActiveItems(data.activeItems);
@@ -13,28 +24,44 @@ const BuyForm = ({ setActiveItems }) => {
       });
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    getActiveItems(keywords);
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <Grid item>
-        <TextField
-          id="keyword-input"
-          label="Keywords"
-          value={keywords}
-          onChange={(event) => setKeywords(event.target.value)}
-          required
-        />
-      </Grid>
-      <Grid item>
-        <Button variant="contained" type="submit">
-          Submit
-        </Button>
-      </Grid>
-    </form>
+      <form onSubmit={handleSubmit}>
+        <Grid item>
+          <TextField
+            id="keyword-input"
+            label="Keywords"
+            value={keywords}
+            onChange={(event) => setKeywords(event.target.value)}
+            required
+          />
+        </Grid>
+        <Grid item>
+          <FormControl component="fieldset">
+            <FormLabel component="legend">Condition</FormLabel>
+            <RadioGroup
+              aria-label="condition"
+              name="condition-radio-buttons-group"
+              value={conditionValue}
+              onChange={(event) => {
+                setConditionValue(event.target.value);
+              }}
+            >
+              <FormControlLabel value="new" control={<Radio />} label="New" />
+              <FormControlLabel value="used" control={<Radio />} label="Used" />
+              <FormControlLabel
+                value="all"
+                control={<Radio />}
+                label="View All"
+              />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" type="submit">
+            Submit
+          </Button>
+        </Grid>
+      </form>
   );
 };
 
--- pages/api/activeitems.js
+++ pages/api/activeitems.js
@@ -1,31 +1,50 @@
 import axios from "axios";
 
+const getConditionValue = (condition) => {
+  if (condition === "new") {
+    return 1000;
+  } else if (condition === "used") {
+    return 3000;
+  }
+  return null;
+};
+
 export default function handler(req, res) {
   return new Promise((resolve) => {
     if (req.method === "GET") {
-      //TODO SUPport used condition product searches(right now only NEW, Buy it now, products are returned)
-      axios
-        .get(
-          `https://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=${process.env.EBAY_APP_ID}&OPERATION-NAME=findItemsByKeywords&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=${req.query.keywords}&GLOBAL-ID=EBAY-US&itemFilter(0).name=Condition&itemFilter(0).value=1000&itemFilter(1).name=ListingType&itemFilter(1).value=FixedPrice`
-        )
-        .then((response) => {
-          if (response.status === 200) {
-            if (
-              response.data.findItemsByKeywordsResponse[0].ack[0] === "Success" ||
-              response.data.findItemsByKeywordsResponse[0].ack[0] === "Warning"
-            ) {
-              res.status(200).json({ activeItems: response.data.findItemsByKeywordsResponse[0].searchResult[0].item });
-            } else {
-              res.status(400).json({ ack: response.data.findItemsByKeywordsResponse[0].ack[0] });
-            }
-            return resolve();
+      let ebayEndpoint = `https://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=${process.env.EBAY_APP_ID}&OPERATION-NAME=findItemsByKeywords&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&buyerPostalCode=60118&keywords=${req.query.keywords}&GLOBAL-ID=EBAY-US&itemFilter(0).name=ListingType&itemFilter(0).value=FixedPrice`;
+      
+      const conditionValue = getConditionValue(req.query.condition);
+      if (conditionValue) {
+        ebayEndpoint = ebayEndpoint.concat(`&itemFilter(1).name=Condition&itemFilter(1).value=${conditionValue}`);
+      }
+
+      axios.get(ebayEndpoint).then((response) => {
+        if (response.status === 200) {
+          if (
+            response.data.findItemsByKeywordsResponse[0].ack[0] === "Success" ||
+            response.data.findItemsByKeywordsResponse[0].ack[0] === "Warning"
+          ) {
+            res.status(200).json({
+              activeItems:
+                response.data.findItemsByKeywordsResponse[0].searchResult[0]
+                  .item,
+            });
           } else {
-            res.status(response.status).json({ error: response.errorMessage });
-            return resolve();
+            res.status(400).json({
+              ack: response.data.findItemsByKeywordsResponse[0].ack[0],
+            });
           }
-        });
+          return resolve();
+        } else {
+          res.status(response.status).json({ error: response.errorMessage });
+          return resolve();
+        }
+      });
     } else {
-      res.status(405).json({ error: "This endpoint only accepts GET requests" });
+      res
+        .status(405)
+        .json({ error: "This endpoint only accepts GET requests" });
       return resolve();
     }
   });
