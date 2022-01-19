9c0f1afeb1500c2c1e41b9b3211a328f5e7aff53
banueloe <ernestobanuelos14@gmail.com>
2022-01-18 22:35:12 -0600

    Move Head to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import Header from "../components/header";
+import Head from "next/head";
 import "../App.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Resale Heaven</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <Header />
-      <div className= "header"></div>
+      <div className="header" />
       <Component {...pageProps} />
     </>
   );
