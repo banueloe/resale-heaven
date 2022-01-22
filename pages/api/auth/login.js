a64fa1c9471911a1c096dc7e587cc33a295d9a2c
banueloe <ernestobanuelos14@gmail.com>
2022-01-21 22:17:03 -0600

    use iron-sessions for user sessions

---
+++ pages/api/auth/login.js
@@ -0,0 +1,39 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+import { sessionOptions } from "../../../lib/session";
+const EbayAuthToken = require("ebay-oauth-nodejs-client");
+
+export default withIronSessionApiRoute(async function loginRoute(req, res) {
+  if (req.query.code) {
+    const ebayAuthToken = new EbayAuthToken({
+      clientId: process.env.EBAY_APP_ID,
+      clientSecret: process.env.EBAY_CERT_ID,
+      redirectUri: process.env.EBAY_RUNAME,
+    });
+    // Exchange Code for Authorization token
+    await ebayAuthToken
+      .exchangeCodeForAccessToken("PRODUCTION", req.query.code)
+      .then(async (data) => {
+        let dataObject = JSON.parse(data);//Todo use refresh token
+        if (dataObject.access_token) {
+          //get auth token then:
+          req.session.user = {
+            token: dataObject.access_token,
+          };
+          await req.session.save().then(() => {
+            console.log("this is a test123");
+            res.redirect("/home");
+          });
+        } else {
+          res.redirect("/");
+          return resolve();
+        }
+      })
+      .catch((error) => {
+        console.log(`Error getting Access token :${JSON.stringify(error)}`);
+        res.redirect("/");
+      });
+  } else {
+    console.log("Missing code");
+    res.redirect("/");
+  }
+}, sessionOptions);
