a935d62c09c3c5df340ab4871146c9f6bf90dc6c
banueloe <ernestobanuelos14@gmail.com>
2022-01-22 10:30:15 -0600

    Pass loggedIn rather than token itself

--- pages/home.js
+++ pages/home.js
@@ -4,63 +4,61 @@ import { useRouter } from "next/router";
 import BookSale from "../public/booksale.jpg";
 
 import { withIronSessionSsr } from "iron-session/next";
-import {sessionOptions} from "../lib/session";
-
+import { sessionOptions } from "../lib/session";
 
 export const getServerSideProps = withIronSessionSsr(
   async function getServerSideProps({ req }) {
     return {
       props: {
-        user: req.session.user,
+        loggedIn: req.session.user.token ? true : false,
       },
     };
   },
   sessionOptions
 );
 
-const Home = ({ user }) => {
-  console.log(user);
+const Home = ({ loggedIn }) => {
+  console.log(loggedIn);
   const router = useRouter();
-    return (
-      <>
-        <Grid container>
-          <Grid item xs={4}>
-            <MediaCard
-              title="Buy"
-              description="test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsum"
-              image={BookSale}
-            />
-          </Grid>
-          <Grid item xs={4}>
-            <MediaCard
-              title="Buy"
-              description="test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsum"
-              image={BookSale}
-            />
-          </Grid>
-          <Grid item xs={4}>
-            <MediaCard
-              title="Buy"
-              description="test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsum"
-              image={BookSale}
-            />
-          </Grid>
+  return (
+    <>
+      <Grid container>
+        <Grid item xs={4}>
+          <MediaCard
+            title="Buy"
+            description="test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsum"
+            image={BookSale}
+          />
         </Grid>
-      </>
-    );
-    // return (
-    //   <Grid container alignItems="center" justify="center" direction="column">
-    //     <Grid item>
-    //       <h>Authentication Error</h>
-    //     </Grid>
-    //     <h2>
-    //       Authentication is required to use certain features in resale-heaven.
-    //       Please try again.
-    //     </h2>
-    //     <Button>Sign in</Button>
-    //   </Grid>
-    // );
-  
+        <Grid item xs={4}>
+          <MediaCard
+            title="Buy"
+            description="test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsum"
+            image={BookSale}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <MediaCard
+            title="Buy"
+            description="test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsum test lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsumtest lorem ipsum"
+            image={BookSale}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+  // return (
+  //   <Grid container alignItems="center" justify="center" direction="column">
+  //     <Grid item>
+  //       <h>Authentication Error</h>
+  //     </Grid>
+  //     <h2>
+  //       Authentication is required to use certain features in resale-heaven.
+  //       Please try again.
+  //     </h2>
+  //     <Button>Sign in</Button>
+  //   </Grid>
+  // );
 };
 
 export default Home;
