d45a058865a22e0f910ac3255ec78c2a26061178
banueloe <ernestobanuelos14@gmail.com>
2022-01-22 10:23:26 -0600

    Add mui media card

---
+++ components/media-card.jsx
@@ -0,0 +1,33 @@
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  CardActionArea,
+} from "@mui/material";
+import Image from "next/image";
+const MediaCard = ({title, description, image }) => {
+  return (
+    <Card sx={{ maxWidth: 400 }}>
+      <CardActionArea>
+        <Image
+          src={image}
+          alt="Category image"
+          layout="responsive"
+          height="400"
+          width="400"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default MediaCard;
