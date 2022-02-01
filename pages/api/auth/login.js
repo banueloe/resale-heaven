5a2d1f08b75eca9380761d279d2c484ec66b5e74
banueloe <ernestobanuelos14@gmail.com>
2022-01-31 21:32:18 -0600

    testing redirect

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -27,7 +27,9 @@ export default withIronSessionApiRoute(async function loginRoute(req, res) {
       password: req.body.password,
     };
     await req.session.save();
-    res.redirect(authLink);
+    res.redirect(
+      "https://auth.ebay.com/oauth2/authorize?client_id=ErnestoB-resalehe-PRD-ca0e5e2a1-7048ac66&redirect_uri=Ernesto_Banuelo-ErnestoB-resale-oycbeqs&response_type=code&scope=https://api.ebay.com/oauth/api_scope%20https://api.ebay.com/oauth/api_scope/sell.inventory.readonly%20https://api.ebay.com/oauth/api_scope/sell.inventory"
+    );
   } else {
     res
       .status(400)
