d01324d46cb6e07129eac5167d4cf2da8be9bd67
banueloe <ernestobanuelos14@gmail.com>
2022-01-31 22:14:24 -0600

    Save logged in user to session

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -7,8 +7,7 @@ export default withIronSessionApiRoute(async function loginRoute(req, res) {
 
     // get user from database then:
     req.session.user = {
-      id: 230,
-      admin: true,
+      username: username,
     };
     await req.session.save();
     res
