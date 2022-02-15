deac0dc2f655ace5b6f687d8cb6acad671ed5d6a
banueloe <ernestobanuelos14@gmail.com>
2022-02-14 21:42:17 -0600

    Create Date range picker component

---
+++ components/date-range-picker.jsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import DateRangePicker from '@mui/lab/DateRangePicker';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import Box from '@mui/material/Box';
+
+export default function CalendarRangePicker({dateRange, setDateRange}) {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <DateRangePicker
+        startText="Check-in"
+        endText="Check-out"
+        value={dateRange}
+        onChange={(newDateRange) => {
+          setDateRange(newDateRange);
+        }}
+        renderInput={(startProps, endProps) => (
+          <React.Fragment>
+            <TextField {...startProps} />
+            <Box sx={{ mx: 2 }}> to </Box>
+            <TextField {...endProps} />
+          </React.Fragment>
+        )}
+      />
+    </LocalizationProvider>
+  );
+}
