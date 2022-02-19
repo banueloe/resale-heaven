8538004d87235b0429e7c50443d47c756ea911a8
banueloe <ernestobanuelos14@gmail.com>
2022-02-19 11:50:20 -0600

    Display inventory items in location tab

--- components/tabs/inventory-locations-tab.js
+++ components/tabs/inventory-locations-tab.js
@@ -1,13 +1,12 @@
-import {
-  Grid,
-  Button,
-  Typography,
-  Card,
-  CardContent,
-} from "@mui/material";
+import { Grid, Button, Typography, Card, CardContent } from "@mui/material";
 
 const InventoryLocationsTab = ({ inventoryLocations }) => {
-  //TODO display associated items
+  inventoryLocations = inventoryLocations.map((location) => {
+    let items = "";
+    location.inventoryItems.forEach((item) => (items += ` ${item},`));
+    return { ...location, items: items.slice(0, -1) };
+  });
+  
   return (
     <Grid container spacing={2}>
       <Grid item xs={2} />
@@ -23,7 +22,10 @@ const InventoryLocationsTab = ({ inventoryLocations }) => {
 
         <Grid container direction="row">
           {inventoryLocations.map((location, index) => (
-            <Card key={index} sx={{ border: 1, borderColor: 'gray', width: 400, mr: 8, mb: 8 }}>
+            <Card
+              key={index}
+              sx={{ border: 1, borderColor: "gray", width: 400, mr: 8, mb: 8 }}
+            >
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
                   {location.name}
@@ -31,6 +33,11 @@ const InventoryLocationsTab = ({ inventoryLocations }) => {
                 <Typography variant="body2" color="text.secondary">
                   {location.description}
                 </Typography>
+                {location.items && (
+                  <Typography variant="body2" color="text.secondary">
+                    Inventory Items: {location.items}
+                  </Typography>
+                )}
               </CardContent>
             </Card>
           ))}
