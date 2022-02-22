64a32d5d7180c741b898b8dd73f6ebc295521004
banueloe <ernestobanuelos14@gmail.com>
2022-02-21 23:08:32 -0600

    Bug fix test

--- pages/api/auth/paypal-token.js
+++ pages/api/auth/paypal-token.js
@@ -6,7 +6,7 @@ export default withIronSessionApiRoute(async function loginRoute(req, res) {
   if (req.method !== "GET")
     res.status(405).json({ error: "This endpoint only accepts GET requests" });
 
-  if (!req.session.user || !req.session.user.token || !req.session.user.email )
+  if (!req.session.user || !req.session.user.token || !req.session.user.email)
     res.status(401).json({ error: "Unauthorized: User is not logged in" });
 
   let dataString = "grant_type=client_credentials";
@@ -21,13 +21,17 @@ export default withIronSessionApiRoute(async function loginRoute(req, res) {
       },
     })
     .then(async (data) => {
-      req.session.user = {
-        ...req.session.user,
-        paypal_token: data.data.access_token,
-      };
-      await req.session.save().then(() => {
-        res.status(200).json({ Success: "Paypal token has been stored in user session" });
-      });
+      if (data.data.access_token) {
+        req.session.user = {
+          ...req.session.user,
+          paypal_token: data.data.access_token,
+        };
+        await req.session.save().then(() => {
+          res
+            .status(200)
+            .json({ Success: "Paypal token has been stored in user session" });
+        });
+      }
     })
     .catch((error) => {
       res.status(400).json({ error: `Error getting Access token :${error}` });
